9eff2ab8749307e196a5f5893b84b378898ea8d8
zH <shenzh2012@gmail.com>
2017-09-26 06:01:08 +0800

    remove build

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const program = require('commander')
 const esLoader = require('@std/esm')(module)
 const docs = esLoader('./src/commands/docs').default
 const config = esLoader('./src/commands/config').default
-const build  = esLoader('./src/commands/build').default
 
 program
   .version('0.0.1')
@@ -19,10 +18,5 @@ program
   .command('config <command> [key] [value]')
   .description('config something')
   .action((command, key, value) => config(command, key, value))
-
-program
-  .command('build [plugin]')
-  .description('build something')
-  .action((plugin) => build(plugin))
 
 program.parse(process.argv)
